e78bfbd2d636be2f48e9248373aad7ea0800b360
dhitozarfhan <dhitozarfhan@gmail.com>
2024-05-03 13:41:39 +0700

    if  (current == NULL)

--- activity6/activity6.cpp
+++ activity6/activity6.cpp
@@ -96,6 +96,13 @@ void deleteNode()
 		current = current->next;
 	}
 
+	if (current == NULL)
+	{
+		cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+		return;
+	}
+
+
 	
 
 
