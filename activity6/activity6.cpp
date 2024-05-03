47a19835d20242726290f0a4348c1876f05a2a3a
dhitozarfhan <dhitozarfhan@gmail.com>
2024-05-03 12:29:08 +0700

    step 2: inserrt the new node at the beginning

--- activity6/activity6.cpp
+++ activity6/activity6.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Node
 {
-	int noMha;
+	int noMhs;
 	string name;
 	Node* next;
 	Node* prev;
@@ -14,4 +14,17 @@ Node *START = NULL;
 void addNode()
 {
 	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // Assign value to the data field of the new node cout << "\nEnter the name of the student: "
+	cin >> newNode->name; // Assign value to the data field of the new node
+
+	// insert the new node in the list 
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{// step 2: insert the new node at the beginning 
+
+		
+
+	}
+		
+
 }
